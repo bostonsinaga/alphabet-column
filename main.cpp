0c2d92b88bbb5112dbf9ea72e2c468fdc9358359
bostonsinaga <bostonsinga@gmail.com>
2024-02-01 00:06:57 +0700

    Add 'listenInput' to catch user input with following a format

--- main.cpp
+++ main.cpp
@@ -1,25 +1,61 @@
+#include <iostream>
+#include <vector>
+
+void listenInput(
+    int argc,
+    char *argv[],
+    const std::vector<std::string> &expectedParams,
+    std::vector<std::string> &values
+) {
+    if (expectedParams.size() != values.size()) {
+        values = std::vector<std::string>(expectedParams.size());
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (i + 1 < argc) {
+            for (int j = 0; j < expectedParams.size(); j++) {
+                if (argv[i] == expectedParams.at(j)) {
+                    values[j] = argv[i+1];
+                }
+            }
+        }
+    }
+}
+
 void printError() {
-    
+    std::cerr << "\nInput Error!\n";
+
+    std::cout
+        << "\nPlease enter:\n"
+        << "--convert [INT] --next-count [INT<opt>] --next-difference [INT<opt>] --out [STR<opt>]\n";
 }
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-        if (i < argc - 1) {
-            switch (argv[i]) {
-                case "--convert": {
-                break;}
-                case "--next-count": {
 
-                break;}
-                case "--next-difference": {
+    const char ALP[26] = {
+        'A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    };
 
-                break;}
-                case "--out": {
+    std::vector<std::string> values;
 
-                break;}
-            }
-        }
-        else st
+    listenInput(
+        argc, argv,
+        {
+            "--convert",
+            "--next-count",
+            "--next-difference",
+            "--out"
+        },
+        values
+    );
+
+    // required
+    if (values[0] == "") {
+        printError();
+        return 0;
     }
+
+    return 0;
 }
 
