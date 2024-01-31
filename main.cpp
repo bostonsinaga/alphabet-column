017c23f0fbc512a2a052d46becb2dd83773c2a09
bostonsinaga <bostonsinga@gmail.com>
2024-02-01 00:10:36 +0700

    Add 'isChANum' function

--- main.cpp
+++ main.cpp
@@ -22,6 +22,23 @@ void listenInput(
     }
 }
 
+bool isChANum(char ch) {
+    if (ch == '0' ||
+        ch == '1' ||
+        ch == '2' ||
+        ch == '3' ||
+        ch == '4' ||
+        ch == '5' ||
+        ch == '6' ||
+        ch == '7' ||
+        ch == '8' ||
+        ch == '9'
+    ) {
+        return true;
+    }
+    return false;
+}
+
 void printError() {
     std::cerr << "\nInput Error!\n";
 
