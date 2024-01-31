b4f76fcf13f2d0ec671384fd0a84073e6470910b
bostonsinaga <bostonsinga@gmail.com>
2024-01-31 16:50:05 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+void printError() {
+    
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (i < argc - 1) {
+            switch (argv[i]) {
+                case "--convert": {
+                break;}
+                case "--next-count": {
+
+                break;}
+                case "--next-difference": {
+
+                break;}
+                case "--out": {
+
+                break;}
+            }
+        }
+        else st
+    }
+}
+
