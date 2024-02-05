780757f081bb0f8f845792a91e1761460082c2af
bostonsinaga <bostonsinga@gmail.com>
2024-02-05 22:33:29 +0700

    Globa functions have moved to 'utilities.h'
    Add new '--decimal' parameter to accept alphabet to decimal input

--- main.cpp
+++ main.cpp
@@ -1,85 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
-void listenInput(
-    int argc,
-    char *argv[],
-    const std::vector<std::string> &expectedParams,
-    std::vector<std::string> &values
-) {
-    if (expectedParams.size() != values.size()) {
-        values = std::vector<std::string>(expectedParams.size());
-    }
-
-    for (int i = 1; i < argc; i++) {
-        if (i + 1 < argc) {
-            for (int j = 0; j < expectedParams.size(); j++) {
-                if (argv[i] == expectedParams.at(j)) {
-                    values[j] = argv[i+1];
-                }
-            }
-        }
-    }
-}
-
-int parseInt(std::string &numStr) {
-
-    std::string pureNum;
-    bool isAnyNum = false,
-         isNegative = false;
-
-    for (auto ch : numStr) {
-        if (ch == '0' ||
-            ch == '1' ||
-            ch == '2' ||
-            ch == '3' ||
-            ch == '4' ||
-            ch == '5' ||
-            ch == '6' ||
-            ch == '7' ||
-            ch == '8' ||
-            ch == '9'
-        ) {
-            pureNum += ch;
-            isAnyNum = true;
-        }
-        else if (!isAnyNum && ch == '-') {
-            isNegative = true;
-        }
-    }
-
-    if (pureNum == "") return 0;
-    return std::stoi(pureNum) * (1 - isNegative * 2);
-}
-
-void printError() {
-    std::cerr << "\nInput Error!\n";
-
-    std::cout
-        << "\nPlease enter:\n"
-        << "--convert [INT] --next-count [INT<opt>] --next-difference [INT<opt>] --out [STR<opt>]\n";
-}
+#include "utilities.h"
 
 int main(int argc, char *argv[]) {
-
-    const char ALP[26] = {
-        'A','B','C','D','E','F','G','H','I','J','K','L','M',
-        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-    };
-
     std::vector<std::string> values;
 
     const int
         CONVERT = 0,
-        NEXT_COUNT = 1,
-        NEXT_DIFFERENCE = 2,
-        OUT = 3;
+        DECIMAL = 1
+        NEXT_COUNT = 2,
+        NEXT_DIFFERENCE = 3,
+        OUT = 4;
 
-    listenInput(
+    util::listenInput(
         argc, argv,
         {
             "--convert",
+            "--decimal"
             "--next-count",
             "--next-difference",
             "--out"
@@ -89,18 +27,37 @@ int main(int argc, char *argv[]) {
 
     /** Formation Errors */
 
-    if (values[CONVERT] == "" ||
-        (values[NEXT_COUNT] == "" && values[NEXT_DIFFERENCE] != "")
+    if ((
+            values[CONVERT] == "" &&
+            values[DECIMAL] == ""
+        ) || (
+            values[NEXT_COUNT] == "" &&
+            values[NEXT_DIFFERENCE] != "" &&
+            values[DECIMAL] == ""
+        ) || (
+            values[DECIMAL] != "" && (
+                values[CONVERT] != "" ||
+                values[NEXT_COUNT] != "" ||
+                values[NEXT_DIFFERENCE] != "" ||
+                values[OUT] != ""
+            )
+        )
     ) {
-        printError();
+        util::printError();
+        return 0;
+    }
+
+    // directly process alphabet to decimal
+    if (values[DECIMAL] != "") {
+        alphabetToDecimal(values[DECIMAL]);
         return 0;
     }
 
     /** Minimum Value Errors */
 
-    int numCol = parseInt(values[CONVERT]),
-        alpsTotal = parseInt(values[NEXT_COUNT]),
-        alpsDiff = parseInt(values[NEXT_DIFFERENCE]);
+    int numCol = util::parseInt(values[CONVERT]),
+        alpsTotal = util::parseInt(values[NEXT_COUNT]),
+        alpsDiff = util::parseInt(values[NEXT_DIFFERENCE]);
 
     if (numCol <= 0) {
         std::cerr << "\nError!\nInput '--convert' must be greater than 0.\n";
@@ -133,7 +90,7 @@ int main(int argc, char *argv[]) {
         numCol += alpsDiff;
 
         if (numCol_copy < 26) {
-            alpCols.back() = ALP[numCol_copy];
+            alpCols.back() = util::ALP[numCol_copy];
         }
         else {
             int alp1StIdx = 26;
@@ -141,11 +98,11 @@ int main(int argc, char *argv[]) {
             while (alp1StIdx > 25) {
                 numCol_copy -= 26;
                 alp1StIdx = numCol_copy / 26;
-                alpCols.back() = ALP[numCol_copy % 26] + alpCols.back();
+                alpCols.back() = util::ALP[numCol_copy % 26] + alpCols.back();
                 numCol_copy = alp1StIdx;
             }
 
-            alpCols.back() = ALP[alp1StIdx] + alpCols.back();
+            alpCols.back() = util::ALP[alp1StIdx] + alpCols.back();
         }
     }
 
